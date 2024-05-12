2bc887b45e6dbcd262f20ebe9880c1e196dd5f21
LINDOSA <detelepathsa@gmail.com>
2024-05-12 11:34:23 +0000

    grit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,9 @@
-class apache_fix {
-  exec { 'fix-wordpress':
-    command => '/bin/chmod 755 /var/www/html/wp-content/uploads',
-    path    => '/usr/bin',
-    onlyif  => '/bin/test -d /var/www/html/wp-content/uploads',
-  }
-}
+# Description: This Puppet resource fixes the incorrect 'phpp' string in the specified file.
 
+# Use the `exec` resource to fix the incorrect 'phpp' string
+exec { 'fix-wordpress':
+  # Specify the sed command to replace 'phpp' with 'php' in the specified file
+  command => '/bin/sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  # Specify the path to the sed command
+  path    => '/usr/local/bin/:/bin/',
+}
