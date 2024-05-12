5d278bc51ee102082bf30551ab0912274d2381d8
LINDOSA <detelepathsa@gmail.com>
2024-05-12 10:54:38 +0000

    Nice

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,8 @@
-# File: 0-strace_is_your_friend.pp
-# Language: Puppet
-
-# Define a new class to fix the Apache 500 error
 class apache_fix {
-  # Use the `exec` resource to run a command that fixes the issue
   exec { 'fix-wordpress':
-    command => 'sudo chmod 755 /var/www/html/wp-content/uploads',
+    command => '/bin/chmod 755 /var/www/html/wp-content/uploads',
     path    => '/usr/bin',
+    onlyif  => '/bin/test -d /var/www/html/wp-content/uploads',
   }
 }
+
