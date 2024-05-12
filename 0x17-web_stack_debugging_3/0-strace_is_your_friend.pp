04032afbee683f89cd818273b9ce99729180cd36
LINDOSA <detelepathsa@gmail.com>
2024-05-12 10:41:31 +0000

    now

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,11 @@
-exec { 'sed_failed':
-  command     => '/bin/echo "Error running sed command"',
-  path        => ['/bin'],  # Specify the path where the echo command is located
-  provider    => shell,     # Specify the shell provider for compatibility
-  refreshonly => true,      # Only run this command when explicitly triggered
-}
+# File: 0-strace_is_your_friend.pp
+# Language: Puppet
 
+# Define a new class to fix the Apache 500 error
+class apache_fix {
+  # Use the `exec` resource to run a command that fixes the issue
+  exec { 'fix-wordpress':
+    command => 'sudo chmod 755 /var/www/html/wp-content/uploads',
+    path    => '/usr/bin',
+  }
+}
